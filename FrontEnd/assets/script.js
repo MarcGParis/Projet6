5fbb0722dd1edfa8c25fcf3d6995d3dc61ebf485
MarcGParis <marcguignard@mail.com>
2023-07-21 11:37:18 +0200

    Affichage photos depuis le back-end

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,3 +1,34 @@
+
+
+
 fetch("http://localhost:5678/api/works")
 .then(reponse => reponse.json())
-.then(reponse2 => console.table(reponse2))
+.then(data => {
+    const galleryElement = document.querySelector(".gallery");
+     data.forEach(work => {
+        let figureElement = document.createElement("figure");
+        let imageElement = document.createElement("img");
+        imageElement.src = work.imageUrl;
+        let figcaptionElement = document.createElement("figcaption");
+        figcaptionElement.innerHTML = work.title;
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(figcaptionElement);
+        galleryElement.appendChild(figureElement);
+    });
+})
+
+fetch("http://localhost:5678/api/categories")
+.then(reponse3 => reponse3.json())
+.then(reponse4 => console.table(reponse4))
+
+// const createFigure = (projet) => {
+//     let figureElement = document.createElement("figure");
+//     let imageElement = document.createElement("img");
+//     imageElement.src = "./assets/images/";
+//     let figcaptionElement = document.createElement("figcaption");
+//     figcaptionElement.innerHTML = projet.texte;
+// };
+
+
+
+
