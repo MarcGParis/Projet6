0c21b84d1b057b7f32bb89b5ed366e74d2572339
MarcGParis <marcguignard@mail.com>
2023-07-25 18:44:36 +0200

    Ajout des filtres et mise en page

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,11 +1,8 @@
-
-
-
 fetch("http://localhost:5678/api/works")
 .then(reponse => reponse.json())
 .then(data => {
     const galleryElement = document.querySelector(".gallery");
-     data.forEach(work => {
+    data.forEach(work => {
         let figureElement = document.createElement("figure");
         let imageElement = document.createElement("img");
         imageElement.src = work.imageUrl;
@@ -14,21 +11,19 @@ fetch("http://localhost:5678/api/works")
         figureElement.appendChild(imageElement);
         figureElement.appendChild(figcaptionElement);
         galleryElement.appendChild(figureElement);
-    });
-})
+    })
+});
 
 fetch("http://localhost:5678/api/categories")
-.then(reponse3 => reponse3.json())
-.then(reponse4 => console.table(reponse4))
-
-// const createFigure = (projet) => {
-//     let figureElement = document.createElement("figure");
-//     let imageElement = document.createElement("img");
-//     imageElement.src = "./assets/images/";
-//     let figcaptionElement = document.createElement("figcaption");
-//     figcaptionElement.innerHTML = projet.texte;
-// };
-
-
-
-
+.then(reponse => reponse.json())
+.then(data => {
+    const categoryElement = document.querySelector(".filtre");
+    data.forEach(category => {
+        let buttonElement = document.createElement("button");
+        let pElement = document.createElement("p");
+        
+        categoryElement.appendChild(buttonElement);
+        pElement.innerHTML = category.name;
+        buttonElement.appendChild(pElement);
+    })
+});
