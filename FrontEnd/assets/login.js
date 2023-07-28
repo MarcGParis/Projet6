2a15e2f0609a6ddd02e751f3bbdb117a73c7c26d
MarcGParis <marcguignard@mail.com>
2023-07-28 10:27:06 +0200

    Mise en fonction des filtres

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,20 @@
+// fetch("http://localhost:5678/api//users/login")
+// .then(reponse => reponse.json())
+// .then(data => {})
+    
+// let form = document.querySelector("form");
+// // Ajout d'un écouteur d'événement sur le formulaire pour écouter le submit
+// form.addEventListener("submit", (event) => {
+//     // évite le rechargement de la page
+//     event.preventDefault()
+
+//     let baliseEmail = document.getElementById("email");
+//     let balisePassword = document.getElementById("password");
+    
+//     let valeurEmail = baliseEmail;
+//     if (valeurEmail === "sophie.bluel@test.tld") {
+//         console.log("E-mail est valide");
+//     } else {
+//         console.log("E-mail est invalide");
+//     }
+// });
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -3,11 +3,15 @@ fetch("http://localhost:5678/api/works")
 .then(data => {
     const galleryElement = document.querySelector(".gallery");
     data.forEach(work => {
+        let figcaptionElement = document.createElement("figcaption");
         let figureElement = document.createElement("figure");
         let imageElement = document.createElement("img");
+
         imageElement.src = work.imageUrl;
-        let figcaptionElement = document.createElement("figcaption");
         figcaptionElement.innerHTML = work.title;
+        figureElement.dataset.categoryId = work.categoryId;
+        figureElement.classList.add("work");
+
         figureElement.appendChild(imageElement);
         figureElement.appendChild(figcaptionElement);
         galleryElement.appendChild(figureElement);
@@ -22,8 +26,32 @@ fetch("http://localhost:5678/api/categories")
         let buttonElement = document.createElement("button");
         let pElement = document.createElement("p");
         
+
         categoryElement.appendChild(buttonElement);
         pElement.innerHTML = category.name;
         buttonElement.appendChild(pElement);
+
+        buttonElement.addEventListener("click", function() {
+            console.log(category.id);
+            let works = document.getElementsByClassName("work");
+            Array.from(works).forEach(work =>{
+                if (category.id != work.dataset.categoryId) {
+                    work.style.display="none";
+                }else{
+                    work.style.display="block";
+                }
+            })
+        })
     })
 });
+
+window.addEventListener("load", function(){
+    let tousElement = document.querySelector(".tousElement");
+    console.log(tousElement);
+    tousElement.addEventListener("click", function(){
+        let works = document.getElementsByClassName("work");
+        Array.from(works).forEach(work =>{
+            work.style.display="block";
+        })
+    })
+})
