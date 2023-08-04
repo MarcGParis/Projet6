8cb7348514515cbde8090c0def47c237a54244cc
MarcGParis <marcguignard@mail.com>
2023-08-04 18:53:45 +0200

    Appel fetch de connexion Passage en corps de requête  valeur e-mail et mot de passe

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,20 +1,23 @@
-// fetch("http://localhost:5678/api//users/login")
-// .then(reponse => reponse.json())
-// .then(data => {})
-    
-// let form = document.querySelector("form");
-// // Ajout d'un écouteur d'événement sur le formulaire pour écouter le submit
-// form.addEventListener("submit", (event) => {
-//     // évite le rechargement de la page
-//     event.preventDefault()
+const formElement = document.getElementById("contact");
 
-//     let baliseEmail = document.getElementById("email");
-//     let balisePassword = document.getElementById("password");
-    
-//     let valeurEmail = baliseEmail;
-//     if (valeurEmail === "sophie.bluel@test.tld") {
-//         console.log("E-mail est valide");
-//     } else {
-//         console.log("E-mail est invalide");
-//     }
-// });
+// Ajout d'un écouteur d'événement sur le formulaire pour écouter le submit
+formElement.addEventListener("submit", (event) => {
+    // évite le rechargement de la page
+    event.preventDefault()
+    const emailElement = document.getElementById("email").value;
+    const passwordElement = document.getElementById("password").value;
+    connect(emailElement, passwordElement);
+});
+
+function connect(email, password) {
+    console.log(email);
+    console.log(password);
+    fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'email': email, 'password': password})
+    });
+};
